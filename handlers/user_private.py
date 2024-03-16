8181f2196c6a29c51239604e7127b96740fd6d50
Sckotch59 <sereza.myshkin@gmail.com>
2024-03-16 22:44:30 +0500

    Роутеры, организация проекта, команды

---
+++ handlers/user_private.py
@@ -0,0 +1,15 @@
+from aiogram.filters import CommandStart, Command
+from aiogram import types, Router
+
+
+user_private_router = Router()
+
+
+@user_private_router.message(CommandStart())
+async def start_cmd(message: types.Message):
+    await message.answer('Привет, я виртуальный помощник')
+
+
+@user_private_router.message(Command('menu'))
+async def menu_cmd(message: types.Message):
+    await message.answer("Вот меню:")
